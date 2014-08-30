f8ca2a6e14da62fb5f1c3837a60fa90d1852e3b2
swethamorthala <swethamorthala@gmail.com>
2014-08-30 17:43:50 +0530

    Admin logged in headers

--- application/views/templates/admin_logged_header.php
+++ application/views/templates/admin_logged_header.php
@@ -8,9 +8,21 @@ and open the template in the editor.
 	<title><?php echo $title ?> Income declaration. </title>
 
 	<link rel="stylesheet" type="text/css" href="/id.css">
+	<link rel="stylesheet" type="text/css" href="/tabs.css">
 </head>
 <body>
 	<div class="header">
 		<p>Income Declaration.</p>
-<span class="logout">	<a href="/index.php/logout">LOGOUT</a> </span>
+		<span class="Logout"><a href="/index.php/logout"></a></span>
+	</div>
+	<div class="header-tabs">
+		<ul class="tabs">
+			<li><a href="/index.php/logout">LOGOUT</a></li> 
+			<li><a href="/index.php/employeelist">LIST OF EMPLOYEE</a></li>
+			<li><a href="/index.php/upload">UPLOAD CTC</a></li>
+			<li><a href="/index.php/employee">ADD EMPLOYEE</a></li>
+			<li><a href="/index.php/home">HOME</a></li>
+		
+		</ul>
+
 	</div>
