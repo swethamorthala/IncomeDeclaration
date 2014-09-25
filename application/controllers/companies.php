3dd423c7a04adf1fac90fc5ac4c441d5aeedce7d
swethamorthala <swethamorthala@gmail.com>
2014-09-25 10:53:25 +0530

    axasx

--- application/controllers/companies.php
+++ application/controllers/companies.php
@@ -12,13 +12,18 @@
 class Companies extends CI_Controller {
     //put your code here
 
-	public function  __construct() {
-		parent::__construct();
-		$this->load->model('register_model');
+	public function  __construct()
+         {
+		parent :: __construct();
+		
+                $this->load->model('register_model');
+                $this->load->model('login_model');
+                
 	}
 
-	public function index() {
-		//$this->load->helper('url');
+	public function index()
+        {
+		$this->load->helper('url');
 		$this->load->library('form_validation');
 
 		$currenturl =$this->uri->segment(1);
@@ -28,43 +33,62 @@ class Companies extends CI_Controller {
 		$data['company_id'] = $company->id;
 		$data['company_name'] = $company->company_name;
 		$data['submission_url'] = $currenturl.'/login';
-
-		$this->form_validation->set_rules('user_name', 'User Name or Email', 'required|xss_clean');
+                $user_name=  $this->input->post('user_name');
+                    
+		$this->form_validation->set_rules('user_name', 'Username', 'xss_clean');
 		$this->form_validation->set_rules('password', 'Password', 'required');
-
-		if($this->form_validation->run() === FALSE) {
+		
+                if($this->form_validation->run() === FALSE) {
 			$this->load->view('templates/header', $data);
 			$this->load->view('login/login', $data);
 			$this->load->view('templates/footer', $data);
-		} else {
-			//Check if the username is email or user name
-			//If username call getEmployee with username and password
-			//If email call getEmployee with username and password
-			//If employee is null, then show errors saying invalid username or password
-			//If employee exists then save employee into the session
-			//Need to return the employee here so that we can decide what is his user type and save the employee info in session
-			$loggedin = $this->register_model->login($company->id, $this->input->post('user_name'), md5($this->input->post('password')));
-			if($loggedin) {
+		} 
+                else {
+                    
+          $Isemail=$this->Isemail($user_name);
+    if($Isemail){
+      
+        $employee = $this->login_model->getEmpfromemail($company->id, $user_name,$password=md5($this->input->post('password')));                  
+                  
+              }    
+     else{
+                  $employee = $this->login_model->getEmpfromusername($company->id, $user_name,$password=md5($this->input->post('password')));
+             }                   
+                
+       if($employee && $employee->id) {
 				$this->load->library('session');
+                        	
+                               
 				$this->session->set_userdata('company_id',$company->id);
 				$this->session->set_userdata('company_name',$company->company_name);
 				$this->session->set_userdata('company_url',$currenturl);
-				$this->session->set_userdata('isUserLoggedIn',TRUE);
-
-		
-				
-				
-				redirect('/'.$currenturl.'/profile', 'location', 301);
-			} else {
-				$data['login_failed'] = 'true';
+			        $this->session->set_userdata('isUserLoggedIn',TRUE);     
+                                 
+                                  redirect('/employeehome');
+                    }
+          
+       
+                        else 
+                  
+                           {
+                               $data['login_failed'] = 'True';
 				$this->load->view('templates/header', $data);
 				$this->load->view('login/login', $data);
 				$this->load->view('templates/footer', $data);
 			}
 
-		}
+             }
+        
 
-		
-	}
+        }
+    public  function Isemail($user_name ){
+      if(filter_var($user_name, FILTER_VALIDATE_EMAIL)) 
+        { 
+          return true;
+        } 
+        else
+            { return false; }
+    }
+ 
 }
 ?>
